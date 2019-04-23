f6a99de389097aac269e356a233d329bf1507be2
Dennis Tang <dennistang.cm98g@nctu.edu.tw>
2019-04-23 09:11:39 +0800

    Workaround for model file parser.

--- yad2k.py
+++ yad2k.py
@@ -77,7 +77,7 @@ def _main(args):
     print('Loading weights.')
     weights_file = open(weights_path, 'rb')
     weights_header = np.ndarray(
-        shape=(5, ), dtype='int32', buffer=weights_file.read(20))
+        shape=(4, ), dtype='int32', buffer=weights_file.read(16))
     print('Weights Header: ', weights_header)
     # TODO: Check transpose flag when implementing fully connected layers.
     # transpose = (weight_header[0] > 1000) or (weight_header[1] > 1000)
